638801171381fa39d95e5a7eccc5deb5c6f18072
Vincent Yang <vincent.yang@isobar.com.au>
2016-10-16 17:32:51 +1100

    Added user event example

--- public/app.jsx
+++ public/app.jsx
@@ -1,9 +1,17 @@
 var Greater = React.createClass({
     // Set default value for properties
     getDefaultProps: function () {
-        return{
+        return {
             name: "React"
         }
+    },
+    onButtonClick: function (e) {
+        e.preventDefault();
+
+        var name  = this.refs.name.value;
+
+        alert(name);
+
     },
     render: function () {
         var name = this.props.name;
@@ -12,6 +20,12 @@ var Greater = React.createClass({
             <div>
                 <h1>Hello {name}!</h1>
                 <p>This is form a component!</p>
+
+                <form onSubmit={this.onButtonClick}>
+                    <input type="text" ref="name" />
+                    <button>Set Name</button>
+                </form>
+
             </div>
         );
     }
